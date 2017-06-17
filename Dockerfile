00abe42bc45392055acad7f360146b5549fef748
Tobias Bradtke <webwurst@gmail.com>
2017-06-17 16:52:27 +0200

    First try

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM python:3.6-alpine
 RUN apk --no-cache add tini curl \
   && pip install --no-cache-dir \
     "Flask~=0.12.2" \
-    "requests~=2.17.3"
+    "requests~=2.17.3" \
+    "minio~=2.2.2"
 
 WORKDIR /usr/src/app
 COPY . /usr/src/app
--- scraper.py
+++ scraper.py
@@ -3,16 +3,27 @@ import re
 import json
 import requests
 import datetime
+from minio import Minio
+import logging
 
 app = Flask(__name__)
-
-import logging
 
 # Log only in production mode.
 # if not app.debug:
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 app.logger.addHandler(stream_handler)
+
+minioClient = Minio('minio.minio.svc:9000',
+                    access_key='minio',
+                    secret_key='minio123')
+                    # secure=True)
+
+try:
+    minioClient.make_bucket("parkleit")
+except ResponseError as err:
+    print(err)
+
 
 # FIXME use default-dict
 
@@ -47,6 +58,13 @@ def scrape():
             })
 
             # FIXME add `coordinates_point` derived from geo_shape if missing
+
+            try:
+                hashid = hashlib.md5(json.dumps(geojson, sort_keys=True)).hexdigest()
+                minioClient.put_object('parkleit', hashid, geojson,
+                                       len(geojson), content_type='application/json')
+            except ResponseError as err:
+                app.logger.error(err)
 
             yield json.dumps(geojson, ensure_ascii=False) + '\n'
 
